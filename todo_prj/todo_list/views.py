e253c4f2f328fe70add355ed9af8456cc0c231dc
Vasil <vrangelov85@abv.bg>
2022-10-01 10:34:04 +0300

    fxed some comments

--- todo_prj/todo_list/views.py
+++ todo_prj/todo_list/views.py
@@ -31,15 +31,15 @@ def view_task(request, pk):
     return render(request, "html_view.html", context)
 
 
-def update_task(request, pk):                            # This will trow the primary key for the task we want to update
-    task = Task.objects.get(id=pk)                       # This is syntax to take a specific object from our Model/Table by id/pk
-    form = TodoForm(instance=task)                       # form will be an instance of the obj we took by its id/pk in Model/Table
+def update_task(request, pk):                  # This will trow the primary key for the task we want to update
+    task = Task.objects.get(id=pk)             # This is syntax to take a specific object from our Model/Table by id/pk
+    form = TodoForm(instance=task)             # form will be an instance of the obj we took by its id/pk in Model/Table
     context = {"form": form}
 
     # This is Update from CRUD
     if request.method == "POST":
-        form = TodoForm(request.POST, instance=task)     # We need to pass the instance otherwise it will create a new obj in the DB
-        if form.is_valid():
+        form = TodoForm(request.POST, instance=task)  # We need to pass the instance otherwise
+        if form.is_valid():                                  # it will create a new obj in the DB
             form.save()
         return redirect("todo_page")
     return render(request, "html_update.html", context)
@@ -49,7 +49,7 @@ def delete_task(request, pk):
     item = Task.objects.get(id=pk)
     context = {"item": item}
 
-    #This is Delete from CRUD
+    # This is Delete from CRUD
     if request.method == "POST":
         item.delete()
         return redirect("todo_page")
